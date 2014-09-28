599c673683ac4cbaa2891132f2b868f0965bbfcd
mleykin <mleykin@mera.ru>
2014-09-28 12:53:05 +0400

    Changed repo structure

--- GemApp/src/com/gemapp/db/models/DBModelInterface.java
+++
@@ -1,8 +0,0 @@
-package com.gemapp.db.models;
-
-import android.content.ContentValues;
-
-public interface DBModelInterface<T> {
-    public T create(ContentValues data);
-    public ContentValues convertToCV();
-}
